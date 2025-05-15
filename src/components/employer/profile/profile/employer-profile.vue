56028d4cff7123f1d8e6fb4627a09ed3bda60579
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-05-15 15:13:41 +0300

    some modifications on employer form

---
+++ src/components/employer/profile/profile/employer-profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container px-4 mb-3">
+    <myProfile/>
+    <media/>
+
+    <button class="btn greenbtn mt-4" style="height: 50px; width:175px">save profile</button>
+  </div>
+</template>
+
+<script setup>
+  import myProfile from './my-profile.vue';
+  import media from './media.vue';
+
+</script>
+
+<style scoped>
+  * {
+    font-family: sans-serif;
+  }
+
+  .greenbtn{
+      border:1px solid transparent;
+      background-color: #5bbb7b;
+      color: white;
+  }
+  .greenbtn:hover{
+      border: 1px #5bbb7b solid;
+      background-color: white;
+      color: #5bbb7b;
+  }
+</style>
