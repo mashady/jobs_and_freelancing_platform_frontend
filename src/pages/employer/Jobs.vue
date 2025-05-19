0585abb718775df1037f5215140e83528dea1dcc
mashady <muhammedmashady@gmail.com>
2025-05-19 21:11:53 +0300

    handle dashboard

--- src/pages/employer/Jobs.vue
+++ src/pages/employer/Jobs.vue
@@ -1,30 +1,9 @@
 <template>
-    <div class="home">
-        <jobs />
-    </div>
+    <Jobs />
 </template>
-
-<<<<<<< HEAD
-<script>
-import jobs from '../../components/employer/Jobs.vue';
-
-export default {
-    components: {
-        jobs
-    }
-};
-</script>
-
-<style scoped>
-</style>
-=======
 <script setup>
-    import Jobs from '../../components/employer/jobs.vue';
+import Jobs from '../../components/employer/jobs.vue';
 
 </script>
 
-<style scoped>
-
-</style>
-    
->>>>>>> 354deb3e22e7488800f1a10d7f0445314e7d1e55
+<style scoped></style>
