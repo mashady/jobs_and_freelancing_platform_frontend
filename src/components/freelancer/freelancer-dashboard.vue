209cb4c925350a6180140ed44803795498b46bf2
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-05-22 19:03:12 +0300

    linking dashboard with backend

--- src/components/freelancer/freelancer-dashboard.vue
+++ src/components/freelancer/freelancer-dashboard.vue
@@ -2,19 +2,24 @@
   <div class="full-screen">
     <h2 class="title text-start mt-3 ms-5 pb-4 fw-bolder">Dashboard</h2>
     <div class="container">
-      <jobStatistics/>
-      <div class="row mt-3">
-        <!-- Chart Column - Takes full width on mobile/tablet, 7/12 on desktop -->
-        <div class="col-12 col-md-8 col-lg-8 rounded rounded-2">
-          <chart style="height:450px; width:100%"/>
-        </div>
-        
-        <!-- Notifications Column - Takes full width on mobile/tablet, 5/12 on desktop -->
-        <div class="col-12 col-md-4 col-lg-3 mt-3 mt-md-0 bg-white">
-          <notifications/>
-        </div>
+      <div v-if="userApplications.length<1">
+        <h2 class="alert alert-info fs-3">No Applied Jobs Yet!</h2>
       </div>
-      <recentOrders class="mt-4" />
+      <div v-else>
+
+        <jobStatistics/>
+        <div class="row mt-3">
+          <div class="col-12 col-md-8 col-lg-8 rounded rounded-2">
+            <chart style="height:450px; width:100%"/>
+          </div>
+          
+          <!-- Notifications Column - Takes full width on mobile/tablet, 5/12 on desktop -->
+          <div class="col-12 col-md-4 col-lg-3 mt-3 mt-md-0 bg-white">
+            <notifications/>
+          </div>
+        </div>
+        <recentOrders class="mt-4" />
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +28,32 @@
 import jobStatistics from './job-statistics.vue';
 import recentOrders from './recentOrders.vue';
 import chart from './chart.vue';
-import notifications from "./notifications.vue"
+import notifications from "./notifications.vue";
+import { ref, onMounted } from 'vue';
+
+const userApplications = ref([]);
+const token = localStorage.getItem('authToken')
+const fetchApplications = async ()=>{
+  const response = await fetch('http://127.0.0.1:8000/api/job-applications',
+    {
+      headers:{
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json'
+      }
+    }
+  )
+
+  const res = await response.json()
+
+  userApplications.value = res.data.data;
+
+
+}
+
+onMounted(() => {
+  fetchApplications();
+})
+
 </script>
 
 <style scoped>
